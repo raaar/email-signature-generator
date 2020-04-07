5b5f15c95be14a20ee1148cb0dacb91235afc5fa
raf <uk.raf.f@gmail.com>
2020-04-07 16:39:01 +0000

    Design updates

--- public/js/form.js
+++ public/js/form.js
@@ -10,35 +10,35 @@ class Form extends HTMLElement {
 
         <form class="js-form">   
             <div class="form-group">
-                <label for="text">Name</label>
-                <input type="text" class="form-control js-input" data-bind="bind-name">
+                <label for="name">Name</label>
+                <input id="name" type="text" class="form-control js-input" data-bind="bind-name">
             </div>
 
 
             <div class="form-group">
-                <label for="email">Role</label>
-                <input type="text" name="" value="" class="form-control js-input" data-bind="bind-role">
+                <label for="role">Role</label>
+                <input id="role" type="text" name="" value="" class="form-control js-input" data-bind="bind-role">
             </div>
 
             <div class="form-group">
-                    <label for="email">Mobile</label>
-                    <input type="text" name="" value="" class="form-control js-input" data-bind="bind-mobile">
+                <label for="mob">Mobile</label>
+                <input id="mob" type="text" name="" value="" class="form-control js-input" data-bind="bind-mobile">
             </div>
 
 
             <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="text" name="" value="" class="form-control js-input js-input-mail-link" data-bind="bind-email">
+                <label for="email">Email</label>
+                <input id="email" type="text" name="" value="" class="form-control js-input js-input-email-link" data-bind="bind-email">
             </div>
 
             <div class="form-group">
-                    <label for="email">Linkedin</label>
-                    <input type="text" name="" value="" class="form-control js-input-link" data-bind="bind-linkedin">
+                <label for="linkdin">Linkedin</label>
+                <input id="linkdin" type="text" name="" value="" class="form-control js-input-link" data-bind="bind-linkedin">
             </div>
 
             <div class="form-group">
-                    <label for="email">Twitter</label>
-                    <input type="text" name="" value="" class="form-control js-input-link" data-bind="bind-twitter">
+                <label for="twitter">Twitter</label>
+                <input id="twitterstar" type="text" name="" value="" class="form-control js-input-link" data-bind="bind-twitter">
             </div>
 
             <button class="btn btn-primary mb-3">Generate</button>
@@ -51,12 +51,25 @@ class Form extends HTMLElement {
         this.shadowRoot.querySelector(".js-form")
             .addEventListener('submit', this._onSubmit.bind(this));
 
+        this.shadowRoot.querySelector(".js-input-email-link")
+            .addEventListener('change', this._onChangeInputEmail.bind(this));
+
+        [...this.shadowRoot.querySelectorAll(".js-input-link")]
+            .map(item => item.addEventListener('change', this._onChangeInputLink.bind(this)));
+
         [...this.shadowRoot.querySelectorAll(".js-input")]
             .map(item => item.addEventListener('change', this._onChange.bind(this)));
     }
     disconnectedCallback() {
         this.shadowRoot.querySelector(".js-form")
             .removeEventListener('submit', this._onSubmit);
+
+        this.shadowRoot.querySelector(".js-input-email-link")
+            .removeEventListener('change', this._onChangeInputEmail);
+
+
+        [...this.shadowRoot.querySelectorAll(".js-input-link")]
+            .map(item => item.removeEventListener('change', this._onChangeInputLink));
 
         [...this.shadowRoot.querySelectorAll(".js-input")]
             .map(item => item.removeEventListener('change', this._onChange));
@@ -69,6 +82,35 @@ class Form extends HTMLElement {
 
         const signatureTarget = e.currentTarget.dataset['bind'];
         document.querySelector(`.${signatureTarget}`).textContent = e.target.value;
+    }
+
+    _onChangeInputLink(e) {
+        if(!e.currentTarget.dataset.hasOwnProperty('bind')) {
+            return;
+        }
+
+        const inputVal = e.target.value;
+        const signatureTarget = e.currentTarget.dataset['bind'];
+
+        document.querySelector(`.${signatureTarget}`).setAttribute('href', inputVal);    
+    }
+
+    _onChangeInputEmail(e) {
+        if(!e.currentTarget.dataset.hasOwnProperty('bind')) {
+            return;
+        }
+
+        const inputVal = e.target.value;
+        const signatureTarget = e.currentTarget.dataset['bind'];
+
+        console.log('emai;', inputVal)
+        document.querySelector(`.${signatureTarget}`).setAttribute('href', inputVal);    
+
+        // $('.js-input-mail-link').keyup(function() {
+        //     var inputVal = $(this).val();
+        //     root.find('.' + $(this).data().bind).attr('href', 'mailto:' + inputVal);
+        //     copyPaster();
+        // });
     }
 
     _onSubmit(e) {
